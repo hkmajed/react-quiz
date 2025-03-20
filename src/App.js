2dbc2cec2bf86784a4e183230942809a306910fe
Hussein Majed <hkmajed@hotmail.com>
2025-03-20 00:02:57 -0400

    developed a json file as a fake api , and started building my quiz application

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
 
+const initialState = {
+  questions: [],
+
+  // loading , error , ready , active, finished
+  status: "loading",
+};
+
+function reducer(state, action) {}
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error("Error"));
+  }, []);
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main className="main">
+        <p>1/15</p>
+        <p>Question</p>
+      </Main>
     </div>
   );
 }
